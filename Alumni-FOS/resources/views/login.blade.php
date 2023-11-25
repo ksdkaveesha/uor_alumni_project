1591e53f2b76a5e40b8be2cc111aad4625c696e4
ksdkaveesha <ksdkaveesha@gmail.com>
2023-11-25 13:52:29 +0530

    login page image added

--- Alumni-FOS/resources/views/login.blade.php
+++ Alumni-FOS/resources/views/login.blade.php
@@ -10,43 +10,48 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins%3A400%2C600%2C700"/>
   <link rel="stylesheet" href="{{asset('css/login.css')}}"/>
 </head>
+
 <body>
-<div class="loginpage-5XP">
-  <div class="auto-group-rkat-48R">
-    <img class="rectangle-4-3Pf" src="./assets/rectangle-4.png"/>
-    <img class="vector-qKX" src="./assets/vector-joo.png"/>
-  </div>
-  <div class="auto-group-c7rm-A6u">
-	<p class="login-G9w">Login</p>
 
-	<div class="group-1-8hw">
-      <img class="vuesax-linear-sms-1Fw" src="./assets/vuesax-linear-sms.png"/>
-	  <input type="text" placeholder="Email" name="email" class="input">
-    </div>
-    <div class="group-2-zdf">
-      <img class="frame-tyw" src="./assets/frame.png"/>
-      <input type="password" placeholder="Password" name="password" class="input">
+    <div class="loginpage-5XP">
+    <div class="auto-group-rkat-48R">
+        <img class="rectangle-4-3Pf" src="{{asset('image/rectangle-4.png')}}"/>
+        <img class="vector-qKX" src="./assets/vector-joo.png"/>
     </div>
 
-	<div class="group-10-wBX">
-		<button class="button">	Log In </button>
-	</div>
+    <!--user name password form-->
+    <div class="auto-group-c7rm-A6u">
+        <p class="login-G9w">Login</p>
 
-	<p class="forgot-your-password-C7T"><a href="">Forgot your password?</a></p>
-    <div class="auto-group-vmb1-uGm">
-      <p class="remember-me-qAR">Remember me</p>
-      <div class="rectangle-10-VVs">
-      </div>
+        <div class="group-1-8hw">
+        <img class="vuesax-linear-sms-1Fw" src="{{asset('image/vuesax-linear-sms.png')}}"/>
+        <input type="text" placeholder="Email" name="email" class="input">
+        </div>
+        <div class="group-2-zdf">
+        <img class="frame-tyw" src="{{asset('image/frame.png')}}"/>
+        <input type="password" placeholder="Password" name="password" class="input">
+        </div>
+
+        <div class="group-10-wBX">
+            <button class="button">	Log In </button>
+        </div>
+
+        <p class="forgot-your-password-C7T"><a href="">Forgot your password?</a></p>
+        <div class="auto-group-vmb1-uGm">
+        <p class="remember-me-qAR">Remember me</p>
+        <div class="rectangle-10-VVs">
+        </div>
+        </div>
+
+        <div class="group-9-pY9">
+        <div class="group-7-a1X">Sign up</div>
+        </div>
+    </div>
+    <div class="group-4-2u7">
+        <p class="or-B1K">OR</p>
+        <img class="subtract-Ha9" src="{{asset('image/subtract.png')}}"/>
+    </div>
+    <img class="vector-ppy" src="{{asset('image/vector.png')}}"/>
     </div>
 
-	<div class="group-9-pY9">
-      <div class="group-7-a1X">Sign up</div>
-    </div>
-  </div>
-  <div class="group-4-2u7">
-    <p class="or-B1K">OR</p>
-    <img class="subtract-Ha9" src="./assets/subtract.png"/>
-  </div>
-  <img class="vector-ppy" src="./assets/vector.png"/>
-</div>
 </body>
