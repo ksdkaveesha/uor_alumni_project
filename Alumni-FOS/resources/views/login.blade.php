f8255c4f8ec2baf1636612e27b9a68ba904aa056
ksdkaveesha <ksdkaveesha@gmail.com>
2023-11-25 12:09:08 +0530

    start log in form

---
+++ Alumni-FOS/resources/views/login.blade.php
@@ -0,0 +1,45 @@
+<html>
+<head></head>
+<body>
+
+    <div style={{width: 1366, height: 768, position: 'relative', background: '#FCF3D2', borderRadius: 54, overflow: 'hidden'}}>
+        <img style={{width: 683, height: 700, left: 649, top: 34, position: 'absolute', borderRadius: 40}} src="https://via.placeholder.com/683x700" />
+        <div style={{left: 241, top: 66, position: 'absolute', color: '#1C1C1C', fontSize: 120, fontFamily: 'Smooch Sans', fontWeight: '700', wordWrap: 'break-word'}}>Login</div>
+        <div style={{width: 364, height: 24, left: 159, top: 555, position: 'absolute'}}>
+          <div style={{left: 160, top: 4, position: 'absolute', color: '#1C1C1C', fontSize: 16, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>OR</div>
+          <img style={{width: 364, height: 1, left: 0, top: 15, position: 'absolute'}} src="https://via.placeholder.com/364x1" />
+        </div>
+        <div style={{width: 364, height: 52, left: 159.50, top: 326.50, position: 'absolute'}}>
+          <div style={{width: 364, height: 52, left: 0, top: 0, position: 'absolute', background: '#FCF3D2', borderRadius: 13, border: '1px #762EA3 solid'}} />
+          <div style={{left: 48, top: 17, position: 'absolute', color: '#1C1C1C', fontSize: 12, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Password</div>
+          <div style={{width: 24, height: 24, left: 18, top: 14, position: 'absolute'}}>
+            <div style={{width: 18, height: 21, left: 3, top: 1, position: 'absolute', background: '#1C1C1C'}}></div>
+          </div>
+        </div>
+        <div style={{width: 364, height: 52, left: 159.50, top: 250.50, position: 'absolute'}}>
+          <div style={{width: 364, height: 52, left: 0, top: 0, position: 'absolute', background: '#FCF3D2', borderRadius: 13, border: '1px #762EA3 solid'}} />
+          <div style={{left: 48, top: 17, position: 'absolute', color: '#1C1C1C', fontSize: 12, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Email</div>
+        </div>
+        <div style={{width: 24, height: 24, left: 174, top: 264, position: 'absolute'}}>
+          <div style={{width: 24, height: 24, left: 0, top: 0, position: 'absolute'}}>
+            <div style={{width: 20, height: 17, left: 2, top: 3.50, position: 'absolute', border: '1.50px black solid'}}></div>
+            <div style={{width: 10, height: 3.12, left: 7, top: 9, position: 'absolute', border: '1.50px black solid'}}></div>
+            <div style={{width: 24, height: 24, left: 0, top: 0, position: 'absolute', opacity: 0}}></div>
+          </div>
+        </div>
+        <div style={{width: 364, height: 52, left: 159.50, top: 402.50, position: 'absolute'}}>
+          <div style={{width: 364, height: 52, left: 0, top: 0, position: 'absolute', background: '#762EA3', borderRadius: 13}} />
+          <div style={{left: 139.50, top: 11.50, position: 'absolute', color: 'white', fontSize: 20, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Log in</div>
+        </div>
+        <div style={{width: 364, height: 52, left: 159, top: 603, position: 'absolute', borderRadius: 13, border: '1px #762EA3 solid'}} />
+        <div style={{width: 136.67, height: 136.67, left: -68.34, top: 567.16, position: 'absolute', background: '#762EA3'}}></div>
+        <div style={{width: 136.67, height: 136.67, left: 1298, top: 98, position: 'absolute', background: '#762EA3'}}></div>
+        <div style={{left: 333, top: 478, position: 'absolute', color: 'rgba(0, 0, 0, 0.73)', fontSize: 16, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}>Forgot  your password?</div>
+        <div style={{left: 367, top: 521, position: 'absolute', color: '#454545', fontSize: 16, fontFamily: 'Poppins', fontWeight: '700', wordWrap: 'break-word'}}>Remember me</div>
+        <div style={{width: 20, height: 20, left: 503, top: 521, position: 'absolute', background: '#FCF3D2', border: '1px #762EA3 solid'}} />
+        <div style={{left: 284, top: 613, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Poppins', fontWeight: '400', wordWrap: 'break-word'}}>Sign up</div>
+      </div>
+
+</body>
+
+</html>
