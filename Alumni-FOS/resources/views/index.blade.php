3b8fa33390c8fe743a9f4424e8b14707b666dbc3
ksdkaveesha <ksdkaveesha@gmail.com>
2023-11-27 20:38:50 +0530

    added nevigatin bar

--- Alumni-FOS/resources/views/index.blade.php
+++ Alumni-FOS/resources/views/index.blade.php
@@ -2,19 +2,64 @@
 <head>
     <link rel="shortcut icon" type="x-icon" href="{{asset('image/faculty-logo.png')}}">
     <title>University of Ruhuna FOS-Alumni</title>
+
+    <!--boostrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!--css-->
+    <link rel="stylesheet" href="{{asset('css/index.css')}}"/>
 </head>
 
 <body style="background-color: #F9F2D8">
 
-    <div style="background-color: #390A40; height:7%; width:100%; position:relative">
-
-            <img src="{{asset('image/uni-logo2.png')}}" style="max-width:20%; position:absolute; top:5px">
-
+    <div style="background-color: #390A40; height:7%; width:100%; position:relative; ">
+        <img src="{{asset('image/uni-logo2.png')}}" style="width:20%; position:absolute; top:13%">         <!--top devition-->
     </div>
 
-    <div style="background-color: #BD6FD8; height:15%; width:100%;">
-        <img src="{{asset('image/FOS logo.png')}}" style="width:20%">
+    <div style="background-color: #BD6FD8; height:15%;  width:100%; position:relative; ">
+        <img src="{{asset('image/FOS logo.png')}}" style="width:20%; position:absolute; top:5%">                       <!--second devition-->
+
+        <div class="nev">
+            <nav style="height: 100%" class="navbar navbar-expand-lg navbar-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                      </div>
+                    </li>
+
+                  </ul>
+                  <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  </form>
+                </div>
+              </nav>
+        </div>
     </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
 
