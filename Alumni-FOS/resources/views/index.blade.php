7ca5eb130c636e90e00066a9f3361f97ddb95fb2
ksdkaveesha <ksdkaveesha@gmail.com>
2023-11-28 00:12:45 +0530

    Recreated index page to desktop

--- Alumni-FOS/resources/views/index.blade.php
+++ Alumni-FOS/resources/views/index.blade.php
@@ -12,50 +12,105 @@
 
 <body style="background-color: #F9F2D8">
 
-    <div style="background-color: #390A40; height:7%; width:100%; position:relative; ">
-        <img src="{{asset('image/uni-logo2.png')}}" style="width:20%; position:absolute; top:13%">         <!--top devition-->
-    </div>
-
-    <div style="background-color: #BD6FD8; height:15%;  width:100%; position:relative; ">
-        <img src="{{asset('image/FOS logo.png')}}" style="width:20%; position:absolute; top:5%">                       <!--second devition-->
-
-        <div class="nev">
-            <nav style="height: 100%" class="navbar navbar-expand-lg navbar-light">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                      </div>
-                    </li>
-
-                  </ul>
-                  <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                  </form>
+    <div style="width: 100%; height: 100%; position: relative; background: #F9F2D8">
+        <div style="left: 0px; top: 0px; position:relative; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
+            <div style="width: 1451px; height: 94px; background: #390A40"></div>
+            <img style="width: 500px; left: 83px; top:13px; position: absolute" src="{{asset('image/uni-logo2.png')}}" />
+        </div>
+        <div style="width: 489px; height: 167px; left: 886px; top: -40px; position: absolute; flex-direction: column; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
+            <div style="width: 577px; height: 167px; background: rgba(41.66, 21.86, 44.61, 0)"></div>
+            <img style="width: 55px;  left: 123px; top: 53px; position: absolute; border-radius: 48px" src="{{asset('image/faculty-logo.png')}}" />
+            <div style="width: 324px; height: 61px; left: 154px; top: 70px; position: absolute; text-align: center; color: #FFC235; font-size: 28px; font-family: Istok Web; font-weight: 700; line-height: 33.60px; word-wrap: break-word">Faculty of Science<br/></div>
+        </div>
+        <div style="width: 1453px; height: 72px; left: 0px; top: 94px; position: absolute">
+            <div style="width: 1453px; height: 72px; left: 0px; top: 0px; position: absolute; background: #D89AEE"></div>
+            <div style="width: 828.74px; height: 29px; left: 114px; top: 31px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 63px; display: inline-flex">
+                <div style="width: 99px; text-align: center; color: #010002; font-size: 24px; font-family: Ibarra Real Nova; font-weight: 700; line-height: 28.80px; word-wrap: break-word">Home</div>
+                <div style="text-align: center; color: #010002; font-size: 24px; font-family: Ibarra Real Nova; font-weight: 700; line-height: 28.80px; word-wrap: break-word">News and Events</div>
+                <div style="text-align: center; color: #010002; font-size: 24px; font-family: Ibarra Real Nova; font-weight: 700; line-height: 28.80px; word-wrap: break-word">Services</div>
+                <div style="text-align: center; color: #010002; font-size: 24px; font-family: Ibarra Real Nova; font-weight: 700; line-height: 28.80px; word-wrap: break-word">About</div>
+            </div>
+            <div style="width: 624px; height: 29px; left: 756px; top: 31px; position: absolute">
+                <div style="width: 274.72px; left: 0px; top: 0px; position: absolute; text-align: center; color: #0D0D0D; font-size: 24px; font-family: IBM Plex Sans Condensed; font-weight: 600; line-height: 28.80px; word-wrap: break-word">Alumni Directory</div>
+                <div style="width: 226.64px; height: 29px; left: 197.21px; top: 0px; position: absolute; text-align: center; color: #0D0D0D; font-size: 24px; font-family: IBM Plex Sans Condensed; font-weight: 600; line-height: 28.80px; word-wrap: break-word">Contact Us</div>
+                <div style="width: 274.72px; left: 349.28px; top: 0px; position: absolute; text-align: center; color: black; font-size: 24px; font-family: IBM Plex Sans Condensed; font-weight: 600; line-height: 28.80px; word-wrap: break-word">Log In | Sign Up</div>
+            </div>
+        </div>
+        <img style="width: 1453px; height: 535px; left: 0px; top: 166px; position: absolute" src="https://via.placeholder.com/1453x535" />
+        <div style="width: 1442px; height: 448px; left: 0px; top: 1576px; position: absolute; background: #390A40; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)">
+            <div style="width: 143px; height: 156px; left: 106px; top: 114px; position: absolute; border: 1px dotted;">
+            <img style="width: 150px;  left: 0px; top: 43px; position: absolute; border-radius: 48px" src="{{asset('image/uni-logo.png')}}" />
+                <div style="left: 140px; top: 40px; position: absolute; text-align: center"><span style="color: rgba(0, 0, 0, 0.20); font-size: 50px; font-family: Baskerville Old Face; font-weight: 400; text-transform: uppercase; word-wrap: break-word">FOS Alumni<br/></span><span style="color: rgba(0, 0, 0, 0.20); font-size: 24px; font-family: Battambang; font-weight: 400; text-transform: uppercase; word-wrap: break-word">UNIVERSITY OF RUHUNA</span></div>
+            </div>
+            <div style="width: 637px; height: 240px; left: 736px; top: 90px; position: absolute">
+                <div style="width: 635px; height: 198px; left: 2px; top: 0px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
+                    <div style="width: 368px; height: 90px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 16px; display: flex">
+                        <div style="justify-content: flex-start; align-items: center; gap: 16px; display: inline-flex">
+                            <div style="width: 24px; height: 24px; position: relative">
+                                <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
+                                <img style="width: 14px; height: 19.47px; left: 5px; top: 2px; position: absolute" src="https://via.placeholder.com/14x19" />
+                            </div>
+                            <div style="color: white; font-size: 20px; font-family: DM Sans; font-weight: 400; line-height: 22px; word-wrap: break-word">Faculty of Science, University of Ruhuna, Matara, Sri Lanka</div>
+                        </div>
+                        <div style="justify-content: flex-start; align-items: flex-start; gap: 120px; display: inline-flex">
+                            <div style="justify-content: flex-start; align-items: center; gap: 16px; display: flex">
+                                <div style="width: 24px; height: 24px; position: relative">
+                                    <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
+                                    <div style="width: 17.96px; height: 17.96px; left: 3.03px; top: 3.01px; position: absolute; background: #188CFF"></div>
+                                </div>
+                                <div style="width: 159px; height: 36px; color: #D0DAF5; font-size: 20px; font-family: Assistant; font-weight: 400; word-wrap: break-word">(123) 456-7890</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div style="justify-content: flex-start; align-items: center; gap: 16px; display: inline-flex">
+                        <div style="width: 24px; height: 24px; position: relative">
+                            <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
+                            <img style="width: 20px; height: 18px; left: 2px; top: 3px; position: absolute" src="https://via.placeholder.com/20x18" />
+                        </div>
+                        <div style="color: #D0DAF5; font-size: 20px; font-family: Assistant; font-weight: 400; word-wrap: break-word">(123) 456-7890</div>
+                    </div>
+                    <div style="width: 199px; height: 36px; left: 0px; top: 134px; position: absolute">
+                        <img style="width: 24px; height: 24px; left: 0px; top: 2px; position: absolute" src="https://via.placeholder.com/24x24" />
+                        <div style="width: 159px; height: 36px; left: 40px; top: 0px; position: absolute; color: #D0DAF5; font-size: 20px; font-family: Assistant; font-weight: 400; word-wrap: break-word">(123) 456-7890</div>
+                    </div>
                 </div>
-              </nav>
+                <div style="width: 399px; height: 53px; left: 0px; top: 187px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
+                    <div style="width: 30px; height: 30px; position: relative">
+                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
+                        <div style="width: 22.50px; height: 22.50px; left: 3.75px; top: 3.75px; position: absolute; background: #188CFF"></div>
+                    </div>
+                    <div style="width: 30px; height: 30px; position: relative; opacity: 0.30">
+                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
+                        <div style="width: 22.50px; height: 18.75px; left: 3.75px; top: 6.25px; position: absolute; background: #2D96E2"></div>
+                    </div>
+                    <div style="width: 30px; height: 30px; position: relative">
+                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
+                        <img style="width: 22.50px; height: 20px; left: 3.75px; top: 5px; position: absolute" src="https://via.placeholder.com/22x20" />
+                    </div>
+                    <div style="width: 30px; height: 30px; position: relative">
+                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
+                        <img style="width: 22.50px; height: 16.25px; left: 3.75px; top: 7.50px; position: absolute" src="https://via.placeholder.com/22x16" />
+                    </div>
+                    <div style="width: 30px; height: 30px; position: relative">
+                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
+                        <img style="width: 22.50px; height: 22.50px; left: 3.75px; top: 3.75px; position: absolute" src="https://via.placeholder.com/22x22" />
+                    </div>
+                    <div style="width: 30px; height: 30px; position: relative">
+                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
+                        <img style="width: 22.50px; height: 13.75px; left: 3.75px; top: 8.75px; position: absolute" src="https://via.placeholder.com/22x14" />
+                    </div>
+                </div>
+            </div>
+            <div style="left: 200px; top: 349px; position: absolute"></div>
+            <div style="left: 132px; top: 338px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 41px; display: inline-flex">
+                <div style="color: #D0DAF5; font-size: 20px; font-family: DM Sans; font-weight: 500; text-transform: uppercase; word-wrap: break-word">About us</div>
+                <div style="color: #D0DAF5; font-size: 20px; font-family: DM Sans; font-weight: 500; text-transform: uppercase; word-wrap: break-word">Contact us</div>
+                <div style="color: #D0DAF5; font-size: 20px; font-family: DM Sans; font-weight: 500; text-transform: uppercase; word-wrap: break-word">Help</div>
+                <div style="color: #D0DAF5; font-size: 20px; font-family: DM Sans; font-weight: 500; text-transform: uppercase; word-wrap: break-word">Privacy Policy</div>
+            </div>
+            <div style="width: 375px; left: 972px; top: 342px; position: absolute"><span style="color: #D0DAF5; font-size: 20px; font-family: Assistant; font-weight: 400; word-wrap: break-word">Copyright © 2023• University of Ruhuna</span><span style="color: #D0DAF5; font-size: 14px; font-family: Assistant; font-weight: 400; word-wrap: break-word">.</span></div>
         </div>
     </div>
-
-
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
