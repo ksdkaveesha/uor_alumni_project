21c948354e0b2004a1876144398a812a856e351c
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2023-12-01 18:46:09 +0530

    Recreated index page
    By pasindu

--- Alumni-FOS/resources/views/index.blade.php
+++ Alumni-FOS/resources/views/index.blade.php
@@ -1,121 +1,114 @@
-<html>
-<head>
-    <link rel="shortcut icon" type="x-icon" href="{{asset('image/faculty-logo.png')}}">
-    <title>University of Ruhuna FOS-Alumni</title>
+<!DOCTYPE html>
+<html lang="en">
 
-    <!--boostrap-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <head>
+        <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+        <link rel="stylesheet" href="{{asset('css/index.css')}}"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Baskervville&family=DM+Sans:opsz,wght@9..40,300&family=Ibarra+Real+Nova:wght@600&family=Inter:wght@800&display=swap"
+            rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
 
-    <!--css-->
-    <link rel="stylesheet" href="{{asset('css/index.css')}}"/>
-</head>
+    <body>
+        <!--Wrapper is the 12-grid system we used-->
 
-<body style="background-color: #F9F2D8">
+        <!--University logos-->
+        <header>
+            <div class="wrapper">
+                <a href="#">
+                    <img src="{{asset('image/indexpage/logo_copy.png')}}" alt="UOR main logo" width="544px" height="72px" style="position: absolute;               
+                    top: 15px;contain: no-repeat;" />
+                </a>
+                <a href="#">
+                    <img src="{{asset('image/indexpage/FOS logo.png')}}" alt="FOS logo" width="489px" height="167px" style="position: absolute; right: 100px;top: -6px; display: 
+                    flex;flex-direction: column" />
+                </a>
 
-    <div style="width: 100%; height: 100%; position: relative; background: #F9F2D8">
-        <div style="left: 0px; top: 0px; position:relative; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 10px; display: inline-flex">
-            <div style="width: 1451px; height: 94px; background: #390A40"></div>
-            <img style="width: 500px; left: 83px; top:13px; position: absolute" src="{{asset('image/uni-logo2.png')}}" />
-        </div>
-        <div style="width: 489px; height: 167px; left: 886px; top: -40px; position: absolute; flex-direction: column; justify-content: center; align-items: center; gap: 10px; display: inline-flex">
-            <div style="width: 577px; height: 167px; background: rgba(41.66, 21.86, 44.61, 0)"></div>
-            <img style="width: 55px;  left: 123px; top: 53px; position: absolute; border-radius: 48px" src="{{asset('image/faculty-logo.png')}}" />
-            <div style="width: 324px; height: 61px; left: 154px; top: 70px; position: absolute; text-align: center; color: #FFC235; font-size: 28px; font-family: Istok Web; font-weight: 700; line-height: 33.60px; word-wrap: break-word">Faculty of Science<br/></div>
-        </div>
-        <div style="width: 1453px; height: 72px; left: 0px; top: 94px; position: absolute">
-            <div style="width: 1453px; height: 72px; left: 0px; top: 0px; position: absolute; background: #D89AEE"></div>
-            <div style="width: 828.74px; height: 29px; left: 114px; top: 31px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 63px; display: inline-flex">
-                <div style="width: 99px; text-align: center; color: #010002; font-size: 24px; font-family: Ibarra Real Nova; font-weight: 700; line-height: 28.80px; word-wrap: break-word">Home</div>
-                <div style="text-align: center; color: #010002; font-size: 24px; font-family: Ibarra Real Nova; font-weight: 700; line-height: 28.80px; word-wrap: break-word">News and Events</div>
-                <div style="text-align: center; color: #010002; font-size: 24px; font-family: Ibarra Real Nova; font-weight: 700; line-height: 28.80px; word-wrap: break-word">Services</div>
-                <div style="text-align: center; color: #010002; font-size: 24px; font-family: Ibarra Real Nova; font-weight: 700; line-height: 28.80px; word-wrap: break-word">About</div>
             </div>
-            <div style="width: 624px; height: 29px; left: 756px; top: 31px; position: absolute">
-                <div style="width: 274.72px; left: 0px; top: 0px; position: absolute; text-align: center; color: #0D0D0D; font-size: 24px; font-family: IBM Plex Sans Condensed; font-weight: 600; line-height: 28.80px; word-wrap: break-word">Alumni Directory</div>
-                <div style="width: 226.64px; height: 29px; left: 197.21px; top: 0px; position: absolute; text-align: center; color: #0D0D0D; font-size: 24px; font-family: IBM Plex Sans Condensed; font-weight: 600; line-height: 28.80px; word-wrap: break-word">Contact Us</div>
-                <div style="width: 274.72px; left: 349.28px; top: 0px; position: absolute; text-align: center; color: black; font-size: 24px; font-family: IBM Plex Sans Condensed; font-weight: 600; line-height: 28.80px; word-wrap: break-word">Log In | Sign Up</div>
-            </div>
-        </div>
-        <img style="width: 1453px; height: 535px; left: 0px; top: 166px; position: absolute" src="https://via.placeholder.com/1453x535" />
-        <div style="width: 1442px; height: 448px; left: 0px; top: 1576px; position: absolute; background: #390A40; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)">
-            <div style="width: 143px; height: 156px; left: 106px; top: 114px; position: absolute; border: 1px dotted;">
-            <img style="width: 150px;  left: 0px; top: 43px; position: absolute; border-radius: 48px" src="{{asset('image/uni-logo.png')}}" />
-                <div style="left: 140px; top: 40px; position: absolute; text-align: center"><span style="color: rgba(0, 0, 0, 0.20); font-size: 50px; font-family: Baskerville Old Face; font-weight: 400; text-transform: uppercase; word-wrap: break-word">FOS Alumni<br/></span><span style="color: rgba(0, 0, 0, 0.20); font-size: 24px; font-family: Battambang; font-weight: 400; text-transform: uppercase; word-wrap: break-word">UNIVERSITY OF RUHUNA</span></div>
-            </div>
-            <div style="width: 637px; height: 240px; left: 736px; top: 90px; position: absolute">
-                <div style="width: 635px; height: 198px; left: 2px; top: 0px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: flex-start; display: inline-flex">
-                    <div style="width: 368px; height: 90px; flex-direction: column; justify-content: flex-start; align-items: flex-start; gap: 16px; display: flex">
-                        <div style="justify-content: flex-start; align-items: center; gap: 16px; display: inline-flex">
-                            <div style="width: 24px; height: 24px; position: relative">
-                                <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
-                                <img style="width: 14px; height: 19.47px; left: 5px; top: 2px; position: absolute" src="https://via.placeholder.com/14x19" />
-                            </div>
-                            <div style="color: white; font-size: 20px; font-family: DM Sans; font-weight: 400; line-height: 22px; word-wrap: break-word">Faculty of Science, University of Ruhuna, Matara, Sri Lanka</div>
-                        </div>
-                        <div style="justify-content: flex-start; align-items: flex-start; gap: 120px; display: inline-flex">
-                            <div style="justify-content: flex-start; align-items: center; gap: 16px; display: flex">
-                                <div style="width: 24px; height: 24px; position: relative">
-                                    <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
-                                    <div style="width: 17.96px; height: 17.96px; left: 3.03px; top: 3.01px; position: absolute; background: #188CFF"></div>
-                                </div>
-                                <div style="width: 159px; height: 36px; color: #D0DAF5; font-size: 20px; font-family: Assistant; font-weight: 400; word-wrap: break-word">(123) 456-7890</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div style="justify-content: flex-start; align-items: center; gap: 16px; display: inline-flex">
-                        <div style="width: 24px; height: 24px; position: relative">
-                            <div style="width: 24px; height: 24px; left: 0px; top: 0px; position: absolute"></div>
-                            <img style="width: 20px; height: 18px; left: 2px; top: 3px; position: absolute" src="https://via.placeholder.com/20x18" />
-                        </div>
-                        <div style="color: #D0DAF5; font-size: 20px; font-family: Assistant; font-weight: 400; word-wrap: break-word">(123) 456-7890</div>
-                    </div>
-                    <div style="width: 199px; height: 36px; left: 0px; top: 134px; position: absolute">
-                        <img style="width: 24px; height: 24px; left: 0px; top: 2px; position: absolute" src="https://via.placeholder.com/24x24" />
-                        <div style="width: 159px; height: 36px; left: 40px; top: 0px; position: absolute; color: #D0DAF5; font-size: 20px; font-family: Assistant; font-weight: 400; word-wrap: break-word">(123) 456-7890</div>
+
+        </header>
+        <!--Navigation bar-->
+        <nav>
+            <div class="wrapper">
+                <div class="navbar">
+                    <div class="menu-item">
+                        <a href="#">Home</a>
+                        <a href="#">News and Events</a>
+                        <a href="#">Sevices</a>
+                        <a href="#">About</a>
+                        <a href="#">Alumni Dirctory</a>
+                        <a href="#">Contact us</a>
+                        <a href="#">Login</a>
+                        <a href="#">Signup</a>
                     </div>
                 </div>
-                <div style="width: 399px; height: 53px; left: 0px; top: 187px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 24px; display: inline-flex">
-                    <div style="width: 30px; height: 30px; position: relative">
-                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
-                        <div style="width: 22.50px; height: 22.50px; left: 3.75px; top: 3.75px; position: absolute; background: #188CFF"></div>
-                    </div>
-                    <div style="width: 30px; height: 30px; position: relative; opacity: 0.30">
-                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
-                        <div style="width: 22.50px; height: 18.75px; left: 3.75px; top: 6.25px; position: absolute; background: #2D96E2"></div>
-                    </div>
-                    <div style="width: 30px; height: 30px; position: relative">
-                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
-                        <img style="width: 22.50px; height: 20px; left: 3.75px; top: 5px; position: absolute" src="https://via.placeholder.com/22x20" />
-                    </div>
-                    <div style="width: 30px; height: 30px; position: relative">
-                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
-                        <img style="width: 22.50px; height: 16.25px; left: 3.75px; top: 7.50px; position: absolute" src="https://via.placeholder.com/22x16" />
-                    </div>
-                    <div style="width: 30px; height: 30px; position: relative">
-                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
-                        <img style="width: 22.50px; height: 22.50px; left: 3.75px; top: 3.75px; position: absolute" src="https://via.placeholder.com/22x22" />
-                    </div>
-                    <div style="width: 30px; height: 30px; position: relative">
-                        <div style="width: 30px; height: 30px; left: 0px; top: 0px; position: absolute"></div>
-                        <img style="width: 22.50px; height: 13.75px; left: 3.75px; top: 8.75px; position: absolute" src="https://via.placeholder.com/22x14" />
-                    </div>
+
+            </div>
+        </nav>
+
+        <main class="hero-section">
+            <div class="wrapper">
+                <!--Main images with slidshows-->
+                <div class="mainimage" style="position: absolute;top: 166px;">
+                    <img src="{{asset('image/indexpage/Main Image.png')}}" class="img-fluid" alt="Slide show">
+                    <h2>Science ALumni<br>Welcome</h2>
+                </div>
+                     
+            </div>
+        </main>
+
+        <!--Learn more part with about us-->
+        <div class="about">
+            <div class="wrapper">
+                <div class="aboutustext">
+                    <p>Welcome to the Alumni Association of Faculty of Science</p>               
+                </div>
+                <div class="btnbox1">
+                    <a class="btn btn-primary" href="#" role="button">Learn More</a>
                 </div>
             </div>
-            <div style="left: 200px; top: 349px; position: absolute"></div>
-            <div style="left: 132px; top: 338px; position: absolute; justify-content: flex-start; align-items: flex-start; gap: 41px; display: inline-flex">
-                <div style="color: #D0DAF5; font-size: 20px; font-family: DM Sans; font-weight: 500; text-transform: uppercase; word-wrap: break-word">About us</div>
-                <div style="color: #D0DAF5; font-size: 20px; font-family: DM Sans; font-weight: 500; text-transform: uppercase; word-wrap: break-word">Contact us</div>
-                <div style="color: #D0DAF5; font-size: 20px; font-family: DM Sans; font-weight: 500; text-transform: uppercase; word-wrap: break-word">Help</div>
-                <div style="color: #D0DAF5; font-size: 20px; font-family: DM Sans; font-weight: 500; text-transform: uppercase; word-wrap: break-word">Privacy Policy</div>
-            </div>
-            <div style="width: 375px; left: 972px; top: 342px; position: absolute"><span style="color: #D0DAF5; font-size: 20px; font-family: Assistant; font-weight: 400; word-wrap: break-word">Copyright © 2023• University of Ruhuna</span><span style="color: #D0DAF5; font-size: 14px; font-family: Assistant; font-weight: 400; word-wrap: break-word">.</span></div>
+      
         </div>
-    </div>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <!--Join with us part-->
+        <div class="join">
+            <div class="wrapper">
+                <div class="join_left">
+                    <div class="parabox">
+                        <p>This isn't a good bye.<br>Its a welcome</p>
+                    </div>
+                    <div class="btnbox">
+                        <a class="btn btn-primary" href="#" role="button">Join with us</a>
+                    </div>
+                </div>
 
-</body>
+                <div class="join_right">
+                    <!--add imagge here-->
+                    <div class="img_box">
+                        <img src="{{asset('image/indexpage/graduwation photo.png')}}" class="img-fluid" alt="graduwationphoto" />
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+        <footer class="footstyle">
+            <div class="wrapper">
+                <p>Hello world</p>
+
+            </div>
+
+        </footer>
+
+        <script type="module" src="welcome.js"></script>
+
+    </body>
 
 </html>
