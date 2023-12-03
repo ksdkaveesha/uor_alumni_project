84f4217173e989d3fd53098f3ae40d7dcbab1038
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2023-12-03 09:29:53 +0530

    Changes in forgot pwd page
    Added responsive image to forgot pwd page.

--- Alumni-FOS/resources/views/forgot_pwd.blade.php
+++ Alumni-FOS/resources/views/forgot_pwd.blade.php
@@ -3,13 +3,16 @@
 
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="Simple Forgot Password Page Using HTML and CSS">
 	<meta name="keywords" content="forgot password page, basic html and css">
     <link rel="stylesheet" href="{{asset('css/forgot_pwd.css')}}"/>
-	<title>Forgot Password Page</title>
+    <!--Bootstrap css link-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	
+    <title>Forgot Password Page</title>
 </head>
-<body style="background-image: url('{{asset('image/forgot_pw/bgimg.png')}}" class="image1">
+<body style="background-image: url('{{asset('image/forgot_pw/bgimg.png')}}" class="img-fluid">
 	<div class="row">
         <img src="{{asset('image/forgot_pw/uni-logo2.png')}}" width="50%" >
 		<h1>Forgot Password</h1>
