b47b934515a9a0c1e7366c0baa00cd1c162aedd8
ksdkaveesha <ksdkaveesha@gmail.com>
2023-12-18 22:14:49 +0530

    Database created

---
+++ Alumni-FOS/app/Http/Controllers/AluminiMemberController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\alumini_member;
+use Illuminate\Http\Request;
+
+class AluminiMemberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(alumini_member $alumini_member)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(alumini_member $alumini_member)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, alumini_member $alumini_member)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(alumini_member $alumini_member)
+    {
+        //
+    }
+}
