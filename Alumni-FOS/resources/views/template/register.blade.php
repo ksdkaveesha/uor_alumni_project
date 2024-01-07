cb1e7f74742c5776720d3d75b1e802ad5596868d
ksdkaveesha <ksdkaveesha@gmail.com>
2024-01-07 16:41:45 +0530

    register form updated

--- Alumni-FOS/resources/views/template/register.blade.php
+++ Alumni-FOS/resources/views/template/register.blade.php
@@ -72,38 +72,107 @@
             <h1 class="contact_text">Contact Us</h1>
          </div>
       </div>
-      <div class="contact_section_2 layout_padding">
-         <div class="container-fluid">
-            <div class="row">
-               <div class="col-md-6 padding_0">
-                  <div class="mail_section">
-                     <div class="email_text">
-                        <div class="form-group">
-                           <input type="text" class="email-bt" placeholder="Name" name="Email">
-                        </div>
-                        <div class="form-group">
-                           <input type="text" class="email-bt" placeholder="Email" name="Email">
-                        </div>
-                        <div class="form-group">
-                           <input type="text" class="email-bt" placeholder="Phone Numbar" name="Email">
-                        </div>
-                        <div class="form-group">
-                           <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="Massage"></textarea>
-                        </div>
-                        <div class="send_btn">
-                           <div type="text" class="main_bt"><a href="#">SEND</a></div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-6 padding_0">
-                  <div class="map-responsive">
-                     <iframe src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France" width="600" height="508" frameborder="0" style="border:0; width: 100%;" allowfullscreen></iframe>
-                  </div>
-               </div>
+
+      <div class="mail_section">
+        <div class="col-md-6 px-0">
+          <div class="form_container">
+            <form action="">
+
+              <div class="form-row">
+                <div class="form-group col">
+                  <input type="text" class="email-bt" placeholder="Your Name" />
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col">
+                  <input type="text" class="email-bt" placeholder="Your Name" />
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col">
+                  <input type="text" class="email-bt" placeholder="Your Name" />
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col">
+                  <input type="text" class="email-bt" placeholder="Your Name" />
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col-lg-5">
+                    <select name="" id="" class="select-bt">
+                      <option value="">Select Service</option>
+                      <option value="">Service 1</option>
+                      <option value="">Service 2</option>
+                      <option value="">Service 3</option>
+                    </select>
+                </div>
+
+                <div class="form-group col-lg-7">
+                  <input type="text" class="email-bt" placeholder="Phone Number" />
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col">
+                    <select name="" id="" class="select-bt">
+                      <option value="">Select Service</option>
+                      <option value="">Service 1</option>
+                      <option value="">Service 2</option>
+                      <option value="">Service 3</option>
+                    </select>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col">
+                    <select name="" id="" class="select-bt">
+                      <option value="">Select Service</option>
+                      <option value="">Service 1</option>
+                      <option value="">Service 2</option>
+                      <option value="">Service 3</option>
+                    </select>
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col">
+                  <input type="text" class="email-bt" placeholder="Your Name" />
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col">
+                  <input type="text" class="email-bt" placeholder="Your Name" />
+                </div>
+              </div>
+
+              <div class="form-row">
+                <div class="form-group col">
+                  <input type="text" class="email-bt" placeholder="Your Name" />
+                </div>
+              </div>
+
+              <div class="send_btn">
+                <div type="text" class="main_bt"><a href="#">SEND</a></div>
+             </div>
+
+            </form>
+          </div>
+        </div>
+        <div class="col-md-6 px-0">
+          <div class="map_container">
+            <div class="map">
+              <div id="googleMap"></div>
             </div>
-         </div>
+          </div>
+        </div>
       </div>
+
       <!--contact section end -->
 
       <!-- footer section start -->
