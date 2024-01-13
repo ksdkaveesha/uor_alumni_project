e4966eb491cc1a08ff81610f36c13da8088e7c24
ksdkaveesha <ksdkaveesha@gmail.com>
2024-01-14 00:27:53 +0530

    basic pages updated

--- Alumni-FOS/resources/views/template/index.blade.php
+++ Alumni-FOS/resources/views/template/index.blade.php
@@ -30,38 +30,41 @@
       <link rel="stylesheet" href="css/owl.theme.default.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
    </head>
-   <body>
+   <body style="background-color: #E7B3F9">
       <!--header section start -->
       <div class="header_section">
          <div class="container-fluid">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-               <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+               <div class="logo"><a href="/"><img src="images/logo.png"></a></div>
                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
                <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav ml-auto">
                      <li class="nav-item active">
-                        <a class="nav-link" href="index.html">Home</a>
+                        <a class="nav-link" href="/">Home</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="news.html">News & Events</a>
+                        <a class="nav-link" href="#testimonial">News & Events</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="about.html">About Us</a>
+                        <a class="nav-link" href="about">About Us</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="directory.html">Alumni Directory</a>
+                        <a class="nav-link" href="directory">Alumni Directory</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="services.html">Services</a>
+                        <a class="nav-link" href="register">Register</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact Us</a>
+                        <a class="nav-link" href="contact">Contact Us</a>
                      </li>
                      <li class="nav-item">
+                        <a class="nav-link" href="login">Log In</a>
+                     </li>
+                     <!--<li class="nav-item">
                         <a class="nav-link" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
-                     </li>
+                     </li>-->
                   </ul>
                </div>
             </nav>
@@ -317,7 +320,7 @@
        services section end -->
 
       <!-- testimonial section start -->
-      <div class="client_section layout_padding">
+      <div class="client_section layout_padding" id="testimonial">
          <div class="container">
             <div class="row">
                <div class="col-sm-12">
--- Alumni-FOS/resources/views/template/update.blade.php
+++ Alumni-FOS/resources/views/template/update.blade.php
@@ -39,30 +39,36 @@
       <div class="header_section">
         <div class="container-fluid">
            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <div class="logo"><a href="index.html"><img src="images/logo.png"></a></div>
+              <div class="logo"><a href="/"><img src="images/logo.png"></a></div>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarNav">
                  <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                       <a class="nav-link" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                       <a class="nav-link" href="about.html">About Us</a>
-                    </li>
-                    <li class="nav-item">
-                       <a class="nav-link" href="gallery.html">Gallery</a>
-                    </li>
-                    <li class="nav-item">
-                       <a class="nav-link" href="services.html">Services</a>
-                    </li>
-                    <li class="nav-item">
-                       <a class="nav-link" href="contact.html">Contact Us</a>
-                    </li>
-                    <li class="nav-item">
-                       <a class="nav-link" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
-                    </li>
+                        <a class="nav-link" href="/">Home</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="#testimonial">News & Events</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="about">About Us</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="directory">Alumni Directory</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="register">Register</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="contact">Contact Us</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="login">Log In</a>
+                     </li>
+                     <!--<li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+                     </li>-->
                  </ul>
               </div>
            </nav>
