fad62ca702c138b0435b9e7d5694ef56d2f5582b
ksdkaveesha <ksdkaveesha@gmail.com>
2024-05-09 22:30:02 +0530

    Update User.php

--- Alumni-FOS/app/Models/User.php
+++ Alumni-FOS/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\alumini_member;
+use App\Models\Admin;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
