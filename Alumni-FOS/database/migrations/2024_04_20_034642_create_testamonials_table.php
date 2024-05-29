b598921136df1a00f60fc0ddf883d22b1d4b822a
ksdkaveesha <ksdkaveesha@gmail.com>
2024-05-29 14:31:19 +0530

    testamonial

--- Alumni-FOS/database/migrations/2024_04_20_034642_create_testamonials_table.php
+++ Alumni-FOS/database/migrations/2024_04_20_034642_create_testamonials_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('testamonials', function (Blueprint $table) {
             $table->id();
             $table->String('name');
+            $table->String('img');
             $table->String('topic');
             $table->String('testamonial');
             $table->timestamps();
