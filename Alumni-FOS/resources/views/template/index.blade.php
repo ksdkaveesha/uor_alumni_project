9475bec17eed764c77e0710de11ea38b98c16e6e
ksdkaveesha <ksdkaveesha@gmail.com>
2024-06-28 10:58:53 +0530

    index testimonial

--- Alumni-FOS/resources/views/template/index.blade.php
+++ Alumni-FOS/resources/views/template/index.blade.php
@@ -306,7 +306,7 @@
                       <div class="col-md-12">
                          <div class="testimonial_section_2">
                             @if($lastThreeRecords_test->count() >= 1)
-                                <h4 class="client_name_text"><img style="max-width: 5%" src="{{asset('storage/'.$lastThreeRecords_test[0]->img)}}" class="image_1"> <span class="quick_icon" ><img src="images/quick-icon.png"></span>&nbsp;&nbsp;&nbsp;{{ $lastThreeRecords_test[0]->topic }}</h4>
+                                <h4 class="client_name_text"><img style="max-width: 5%; border-radius: 50%;" src="{{asset('storage/'.$lastThreeRecords_test[0]->img)}}" class="image_1"> <span class="quick_icon" ><img src="images/quick-icon.png"></span>&nbsp;&nbsp;&nbsp;{{ $lastThreeRecords_test[0]->topic }}</h4>
                                 <h5 class="customer_text"><b>{{ $lastThreeRecords_test[0]->name }}</b></h5>
                                 <p class="customer_text">{{ $lastThreeRecords_test[0]->testamonial }}</p>
                             @else
@@ -327,7 +327,7 @@
                       <div class="col-md-12">
                          <div class="testimonial_section_2">
                             @if($lastThreeRecords_test->count() >= 2)
-                                <h4 class="client_name_text"><img style="max-width: 5%" src="{{asset('storage/'.$lastThreeRecords_test[1]->img)}}" class="image_1"> <span class="quick_icon" ><img src="images/quick-icon.png"></span>&nbsp;&nbsp;&nbsp;{{ $lastThreeRecords_test[1]->topic }}</h4>
+                                <h4 class="client_name_text"><img style="max-width: 5%; border-radius: 50%;" src="{{asset('storage/'.$lastThreeRecords_test[1]->img)}}" class="image_1"> <span class="quick_icon" ><img src="images/quick-icon.png"></span>&nbsp;&nbsp;&nbsp;{{ $lastThreeRecords_test[1]->topic }}</h4>
                                 <h5 class="customer_text"><b>{{ $lastThreeRecords_test[1]->name }}</b></h5>
                                 <p class="customer_text">{{ $lastThreeRecords_test[1]->testamonial }}</p>
                             @else
@@ -348,7 +348,7 @@
                       <div class="col-md-12">
                          <div class="testimonial_section_2">
                             @if($lastThreeRecords_test->count() >= 3)
-                                <h4 class="client_name_text"><img style="max-width: 5%" src="{{asset('storage/'.$lastThreeRecords_test[2]->img)}}" class="image_1"><span class="quick_icon" ><img src="images/quick-icon.png"></span>&nbsp;&nbsp;&nbsp;{{ $lastThreeRecords_test[2]->topic }}</h4>
+                                <h4 class="client_name_text"><img style="max-width: 5%; border-radius: 50%;" src="{{asset('storage/'.$lastThreeRecords_test[2]->img)}}" class="image_1"><span class="quick_icon" ><img src="images/quick-icon.png"></span>&nbsp;&nbsp;&nbsp;{{ $lastThreeRecords_test[2]->topic }}</h4>
                                 <h5 class="customer_text"><b>{{ $lastThreeRecords_test[2]->name }}</b></h5>
                                 <p class="customer_text">{{ $lastThreeRecords_test[2]->testamonial }}</p>
                             @else
